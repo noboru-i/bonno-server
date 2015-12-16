81f2bd1b6c801894d6c92e52c83137cbabd54ef0
noboru-i <ishikura.noboru@gmail.com>
2015-12-16 20:32:27 +0900

    create public dir

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/public/'));
 
 var server = http.createServer(app);
 server.listen(port);
